a7f0a6bae69840b29c7b83cffa84afcebf2eb104
JoMinhwa <minhwa1224@naver.com>
2024-04-03 14:22:55 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 45.38 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    arr = str.split("");
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === arr[i].toUpperCase()) {
+            arr[i] = arr[i].toLowerCase();
+        } else {
+            arr[i] = arr[i].toUpperCase();
+        }
+    }
+    
+    console.log(arr.join(''))
+});
