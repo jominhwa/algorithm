e550b399f9e945ae72517c460045a73297a92d5e
JoMinhwa <minhwa1224@naver.com>
2024-04-08 15:16:58 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,8 +1,8 @@
 function solution(n) {
     var answer = 0;
     
-    for(let i = 0; i <= n; i = i + 2) {
-            n % 2 !== 0 ? answer = answer + (i + 1) : answer = answer + i * i
+    for(let i = 0; i <= n; i += 2) {
+            n % 2 !== 0 ? answer += i + 1 : answer += i * i
     }
     
     return answer;
