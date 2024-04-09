94ad887d7c486538f0927ef1da145652f1708a97
JoMinhwa <minhwa1224@naver.com>
2024-04-09 18:03:55 +0900

    [level 0] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,12 @@
+function solution(code) {
+    var ret = '';
+    var mode = 0;
+    [...code].map((e, i) => {
+        if(mode === 0) {
+            e === "1" ? mode = 1 : (i % 2 ? '' : ret += e);
+        } else {
+            e === "1" ? mode = 0 : (i % 2 ? ret += e : '');
+        }
+    })
+    return !ret ? "EMPTY" : ret;
+}
