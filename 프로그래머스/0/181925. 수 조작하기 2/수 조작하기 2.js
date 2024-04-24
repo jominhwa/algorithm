0a446cbdb700809d59fba5e6a9ade19a20474cea
JoMinhwa <minhwa1224@naver.com>
2024-04-24 16:31:13 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.57 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+const object = {
+    '1': 'w',
+    '-1': 's',
+    '10': 'd',
+    '-10': 'a'
+}
+
+function solution(numLog) {
+    return numLog.map((e, i)=>object[e - numLog[i - 1]] ).join('');
+}
